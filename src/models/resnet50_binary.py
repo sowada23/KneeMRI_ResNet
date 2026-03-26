1a5b8d3381c99ddc65b552c6d1fc429198e402c3
Sora Owada <sowada23@earlham.edu>
2026-03-26 11:31:31 -0400

    Changed to accept 3-ch

--- src/models/resnet50_binary.py
+++ src/models/resnet50_binary.py
@@ -5,8 +5,6 @@ from torchvision import models
 
 def Resnet50(cfg):
     model = models.resnet50(weights=cfg.MODEL_WEIGHT)
-    old_conv = model.conv1  # original: [64, 3, 7, 7]
-    model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
     model.fc = nn.Sequential(
         nn.Dropout(p=0.3),
         nn.Linear(model.fc.in_features, 1)
