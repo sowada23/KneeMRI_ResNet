ceaad890de6bce24dd578e7f9f5320950f62e56d
Sora <soraowada@icloud.com>
2026-03-29 13:57:08 -0400

    Added guusain noise

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -1,6 +1,9 @@
 from torchvision import transforms
 import torch
 
+# def repeat_to_3ch(x):
+#     return x.repeat(3, 1, 1)
+
 class AddGaussianNoise:
     def __init__(self, std=0.01):
         self.std = std
@@ -8,17 +11,14 @@ class AddGaussianNoise:
     def __call__(self, x):
         return x + torch.randn_like(x) * self.std
 
-def repeat_to_3ch(x):
-    return x.repeat(3, 1, 1)
-
 def build_train_transforms(cfg):
     return transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.RandomAffine(degrees=0, translate=(0.02, 0.02), scale=(0.98, 1.02)),
         transforms.Lambda(lambda x: torch.clamp(x, -3.0, 3.0)),
-        transforms.Lambda(repeat_to_3ch),
-        transforms.Normalize(mean=cfg.IMAGENET_MEAN, std=cfg.IMAGENET_STD),
-        AddGaussianNoise(std=0.01),
+        transforms.Normalize(mean=cfg.MEAN, std=cfg.STD),
+        AddGaussianNoise(std=0.01)
+        
     ])
 
 
@@ -26,6 +26,5 @@ def build_eval_transforms(cfg):
     return transforms.Compose([
          transforms.Resize((224, 224)),
          transforms.Lambda(lambda x: torch.clamp(x, -3.0, 3.0)),
-         transforms.Lambda(repeat_to_3ch),
-         transforms.Normalize(mean=cfg.IMAGENET_MEAN, std=cfg.IMAGENET_STD),
+         transforms.Normalize(mean=cfg.MEAN, std=cfg.STD),
     ])
